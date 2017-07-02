0b42e196ffda77a66f5944780d4ce654b8e33900
vgrozev <vgrozev@gmail.com>
2017-07-02 13:41:52 -0400

    6ta

---
+++ Lecture_2/Задача_6_Matchmaking.py
@@ -0,0 +1,90 @@
+people = [
+    {
+        'name': "Мария",
+        'interests': ['пътуване', 'танци', 'плуване', 'кино'],
+        'gender': "female",
+    },
+    {
+        'name': "Диана",
+        'interests': ['мода', 'спортна стрелба', 'четене', 'скандинавска поезия'],
+        'gender': "female",
+    },
+    {
+        'name': "Дарина",
+        'interests': ['танци', 'покер', 'история', 'софтуер'],
+        'gender': "female",
+    },
+    {
+        'name': "Лилия",
+        'interests': ['покер', 'автомобили', 'танци', 'кино'],
+        'gender': "female",
+    },
+    {
+        'name': "Галя",
+        'interests': ['пътуване', 'автомобили', 'плуване', 'баскетбол'],
+        'gender': "female",
+    },
+    {
+        'name': "Валерия",
+        'interests': ['плуване', 'покер', 'наука', 'скандинавска поезия'],
+        'gender': "female",
+    },
+    {
+        'name': "Ина",
+        'interests': ['кино', 'лов със соколи', 'пътуване', 'мода'],
+        'gender': "female",
+    },
+    {
+        'name': "Кирил",
+        'interests': ['баскетбол', 'автомобили', 'кино', 'наука'],
+        'gender': "male",
+    },
+    {
+        'name': "Георги",
+        'interests': ['автомобили', 'футбол', 'плуване', 'танци'],
+        'gender': "male",
+    },
+    {
+        'name': "Андрей",
+        'interests': ['футбол', 'скандинавска поезия', 'история', 'танци'],
+        'gender': "male",
+    },
+    {
+        'name': "Емил",
+        'interests': ['летене', 'баскетбол', 'софтуер', 'наука'],
+        'gender': "male",
+    },
+    {
+        'name': "Димитър",
+        'interests': ['футбол', 'лов със соколи', 'автомобили', 'баскетбол'],
+        'gender': "male",
+    },
+    {
+        'name': "Петър",
+        'interests': ['пътуване', 'покер', 'баскетбол', 'лов със соколи'],
+        'gender': "male",
+    },
+    {
+        'name': "Калоян",
+        'interests': ['история', 'покер', 'пътуване', 'автомобили'],
+        'gender': "male",
+    },
+]
+
+males = []
+females = []
+for list_member in people:
+    if list_member['gender'] == 'male':
+        males.append(list_member)
+    else:
+        females.append(list_member)
+
+#for male in males:
+#    print(male['gender'])
+#for female in females:
+#    print(female['gender'])
+
+for male in males:
+    for female in females:
+        m_interests = set(male['interests']
+        #f_interests = set(female['interests']
