8aec68e469ccc6dde43665fc668a62d22faecfe6
Vladimir Stoayanov <vgrozev@gmail.com>
2017-08-13 12:48:05 -0400

    lec 5

--- Python_3_Programming_January_and_July_2016/Lecture_5/Задача: 1_Анализ_на_данни_от_верига_магазини.py
+++ Python_3_Programming_January_and_July_2016/Lecture_5/Задача: 1_Анализ_на_данни_от_верига_магазини.py
@@ -81,9 +81,7 @@
     "K81938","FR","Nantes","2016-01-14T20:58:38+01:00",36.57
     "41975","IT","Catania","2016-01-12T10:57:50+01:00",409.58
     "538352","DE","Düsseldorf","2015-12-18T20:50:21+01:00",95.03
-
 ==============
-    
 Насоки
     Parse на дати в ISO8601 формат
         За да parse-нете датите във файловете с продажби, използвайте модула iso8601
@@ -114,9 +112,8 @@
         d_in_utc = d.astimezone(timezone.utc)
         print(d_in_utc)
         '2015-12-18 19:50:21+00:00'
-
 """
-from datetime import datetime, timezone
+from datetime import timezone
 import iso8601 as iso_time
 import csv
 import pprint
@@ -130,8 +127,37 @@ SALES_1M = 'sales-analysis-assignment-all/sales-1M.csv'
 SALES_10K = "sales-analysis-assignment-all/sales-10K.csv"
 SALES_100K = 'sales-analysis-assignment-all/sales-100K.csv'
 
-headers = ['id', 'country', 'town', 'date']
+HEADERS = ['id', 'country', 'town', 'date']
 COUNT = 0
+EARLIEST = ''
+LATEST = ''
+
+
+#########################################################
+# Calculate Earliest and Latest dates
+#########################################################
+def min_max_convert_date(date_string: str):
+    global EARLIEST
+    global LATEST
+
+    dt = iso_time.parse_date(date_string)
+
+    if (EARLIEST == '') and (LATEST == ''):
+        EARLIEST = dt
+        LATEST = dt
+
+    EARLIEST = min(EARLIEST, dt)
+    LATEST = max(LATEST, dt)
+
+    # # the one below works correctly, but it is not cool enough :-)
+    # if (LATEST >= dt) and (EARLIEST >= dt):
+    #     EARLIEST = dt
+    # else:
+    #     if LATEST <= dt:
+    #         LATEST = dt
+
+    # # leave only hours and convert it back to string:
+    return str(dt.replace(minute=0, second=0).astimezone(timezone.utc))
 
 
 #########################################################
@@ -146,19 +172,22 @@ def sort_sales(sales_file) -> dict:
 
             global COUNT
             COUNT += 1
-            row[-1] = float(row[-1])  # convert the price to float
-            # leave only hours and convert it back to string:
-            row[-2] = str(iso_time.parse_date(row[-2]).replace(minute=0, second=0))
+
+            # convert the price to float
+            row[-1] = float(row[-1])
+
+            # send the date string to min/max/convert function to check for min/mac and convert it to apr. format
+            row[-2] = min_max_convert_date(row[-2])
 
             for i, column in enumerate(row[:-1]):  # loop trough the list excluding the last element, which is 'price'
-                if column in final_sales_dict.get(headers[i], {}):
+                if column in final_sales_dict.get(HEADERS[i], {}):
                     # if the key for the inner dictionary already exist, sum prices:
-                    value = final_sales_dict[headers[i]][column] + row[-1]
+                    value = final_sales_dict[HEADERS[i]][column] + row[-1]
                 else:
                     value = row[-1]
 
                 # add resulting price as value to a key/pair in the inner dict
-                final_sales_dict[headers[i]][column] = round(value, 2)
+                final_sales_dict[HEADERS[i]][column] = value
 
     return final_sales_dict
 
@@ -170,45 +199,75 @@ def sum_total(dict_to_sum: dict) -> float:
     for sub_dict in dict_to_sum.items():
         values = [v for v in sub_dict[1].values()]  # create a list of the second values of the tuple 'sub_dict'
 
-    return round(sum(values), 2)
+    return sum(values)
+
+
+#########################################################
+# Match categories by ID
+#########################################################
+def categories_by_id(data_dict) -> dict:
+    temp_dict = dict()
+    with open(CATALOG, 'r', newline='', encoding='utf-8') as catalog:
+        reader = csv.reader(catalog)
+        for row in reader:  # every line of the file as a list
+            if row[0] in data_dict:     # if the ID from the Catalog exists in the current ID dict
+                if row[5] in temp_dict:     # if the category field key already in the temp dict
+                    temp_dict[row[5]] += data_dict[row[0]]  # add to what is in the corresponding value
+                else:
+                    temp_dict[row[5]] = data_dict[row[0]]  # else, assign the new key and its value from the ID dict
+
+    return temp_dict
 
 
 #########################################################
 # list top 5 of provided criteria
 #########################################################
-def top_five(criteria: str, data_dict) -> list:
-    data_price_tuples = [(price, data) for data, price in data_dict[criteria].items()]
+def top_five(data_dict) -> list:
+    data_price_tuples = [(price, data) for data, price in data_dict.items()]
     data_price_tuples.sort(reverse=True)
-    top_5_list = [(t[1], t[0]) for t in data_price_tuples[:5]]  #reverse them so the town is first
-    print(top_5_list)
+    top_5_list = [(t[1], t[0]) for t in data_price_tuples[:5]]  # reverse them so the town is
+
     return top_5_list
 
-def convert_to_UTC(data_dict) -> dict:
 
-
-    return ""
+#########################################################
+# Main section (driver). It can be a main method
+#########################################################
 
 sorted_sales_dict = sort_sales(SALES_10K)
+# sorted_sales_dict = sort_sales(SALES_100K)
+# sorted_sales_dict = sort_sales(SALES_1M)
+
 # pprint.pprint(sorted_sales_dict)  # pretty print for nested dictionaries
 total = sum_total(sorted_sales_dict)
-average = round(total / COUNT, 2)
-top_5_towns = top_five(headers[2], sorted_sales_dict)
+average = total / COUNT
 
+category_by_id = categories_by_id(sorted_sales_dict[HEADERS[0]])
+top_5_categories = top_five(category_by_id)
 
-top_5_dates = top_five(headers[3], sorted_sales_dict)
+top_5_towns = top_five(sorted_sales_dict[HEADERS[2]])
 
-print('Обобщение')
+top_5_dates = top_five(sorted_sales_dict[HEADERS[3]])  # without conversion to UTC
+
+# ###  Prints, part of the Main section ### #
+print('\n\nОбобщение')
 print('---------')
 print('    Общ брой продажби: ', COUNT)
-print('    Общо сума продажби: {}'.format(total), '\u20ac')
-print('    Средна цена на продажба: {}'.format(average), '\u20ac')
-print("===========  temp ===========")
+print('    Общо сума продажби: {:.2f}'.format(total), '\u20ac')
+print('    Средна цена на продажба: {:.2f}'.format(average), '\u20ac')
+print('    Начало на период на данните: ', str(EARLIEST.isoformat()))
+print('    Край на период на данните: ', str(LATEST.isoformat()), '\n')
+
+print('Сума на продажби по категории(top 5)')
+print('-----------------------------')
+print(''.join(["    %s: %.2f \u20ac \n" % tup for tup in top_5_categories]))
+
 print('Сума на продажби по градове (top 5)')
 print('-----------------------------')
 # convert the tuples to strings and print them separate lines with a ure sign at the end of each
-print(''.join(["    %s: %s \u20ac \n" % tup for tup in top_5_towns]))
-print("===========  temp ===========")
+print(''.join(["    %s: %.2f \u20ac \n" % tup for tup in top_5_towns]))
+
 print('Сума на продажби по час (top 5)')
 print('-----------------------------')
 # convert the tuples to strings and print them separate lines with a ure sign at the end of each
-print(''.join(["    %s: %s \u20ac \n" % tup for tup in top_5_dates]))
+print(''.join(["    %s: %.2f \u20ac \n" % tup for tup in top_5_dates]))
