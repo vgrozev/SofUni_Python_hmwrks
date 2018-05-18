de6bf31c87b8b771592f03b3a5a552fbf23660ee
Vladimir Stoayanov <vgrozev@gmail.com>
2018-05-18 18:45:51 -0400

    novi zadachi

--- Programming Basics with Python - април 2018/06. Simple-Conditions/08. Metric Converter.py
+++ Programming Basics with Python - април 2018/06. Simple-Conditions/08. Metric Converter.py
@@ -1,7 +1,8 @@
 num = float(input())
 from_metric = input()
 to_metric = input()
-
+in_meters = 0
+final_output = 0
 # 1. convert all to meters
 
 if from_metric == 'm':
@@ -10,7 +11,7 @@ elif from_metric == 'mm':
     in_meters = num / 1000
 elif from_metric == 'cm':
     in_meters = num / 100
-elif from_metric =='mi':
+elif from_metric == 'mi':
     in_meters = num / 0.000621371192
 elif from_metric == 'in':
     in_meters = num / 39.3700787
@@ -24,20 +25,20 @@ elif from_metric == 'yd':
 # 2. from meters to end metric
 
 if to_metric == 'm':
-   final_output = in_meters * 1
+    final_output = in_meters * 1
 elif to_metric == 'mm':
-   final_output = in_meters * 1000
+    final_output = in_meters * 1000
 elif to_metric == 'cm':
-   final_output = in_meters * 100
-elif to_metric =='mi':
-   final_output = in_meters * 0.000621371192
+    final_output = in_meters * 100
+elif to_metric == 'mi':
+    final_output = in_meters * 0.000621371192
 elif to_metric == 'in':
-   final_output = in_meters * 39.3700787
+    final_output = in_meters * 39.3700787
 elif to_metric == 'km':
-   final_output = in_meters * 0.001
+    final_output = in_meters * 0.001
 elif to_metric == 'ft':
-   final_output = in_meters * 3.2808399
+    final_output = in_meters * 3.2808399
 elif to_metric == 'yd':
-   final_output = in_meters * 1.0936133
+    final_output = in_meters * 1.0936133
 
 print(final_output, to_metric)
