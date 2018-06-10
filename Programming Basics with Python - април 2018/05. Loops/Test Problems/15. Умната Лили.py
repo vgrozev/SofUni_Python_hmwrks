66319420670e8355bcf41e1b606dca1a5080e5fd
Vladimir Stoayanov <vgrozev@gmail.com>
2018-06-09 20:00:30 -0400

    novi zadachi loop

--- Programming Basics with Python - април 2018/05. Loops/Test Problems/15. Умната Лили.py
+++ Programming Basics with Python - април 2018/05. Loops/Test Problems/15. Умната Лили.py
@@ -4,16 +4,23 @@ toy_price = int(input())
 
 toy_count = 0
 money = 10
-tot_sum = 0
+money_sum = 0
 brother_money = 0
 
 for year in range(1, age + 1):
     if year % 2 == 0:
-        tot_sum += money
+        money_sum += money
         money += 10
         brother_money += 1
     else:
         toy_count += 1
 
-sum = tot_sum + toy_count * toy_price - brother_money
+tot_sum = money_sum + toy_count * toy_price - brother_money
+diff = abs(tot_sum - washer_price)
 
+if tot_sum < washer_price:
+    message = 'No! {:.2f}'.format(diff)
+else:
+    message = 'Yes! {:.2f}'.format(diff)
+
+print(message)
