287b03df088be201e4c54b8ee21808ca88ae03c7
Vladimir Stoayanov <vgrozev@gmail.com>
2018-07-15 23:19:45 -0400

    novi zadachi loop

--- Programming Basics with Python - април 2018/06. Drawing-with-Loops/Test Problems/13. Stop.py
+++ Programming Basics with Python - април 2018/06. Drawing-with-Loops/Test Problems/13. Stop.py
@@ -1,2 +1,13 @@
 n = int(input())
 
+first_line = "." * (n + 1) + "_" * (2 * n + 1) + "." * (n + 1)
+
+print(first_line)
+
+for i in range(n):
+    print("." * (n - i) + "//" + "_" * ((2 * n - 1) + (2 * i)) + "\\\\" + "." * (n - i))
+
+print("//" + "_" * ((4 * n - 6) // 2) + "STOP!" + "_" * ((4 * n - 6) // 2) + "\\\\")
+
+for j in range(n):
+    print("." * j + "\\\\" + "_" * ((4 * n - 1) - (2 * j)) + "//" + "." * j)
