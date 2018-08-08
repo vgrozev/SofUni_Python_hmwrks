c974b7721f0a57df734ed334b9a288b064bc8105
vlado <vgrozev@gmail.com>
2018-08-08 02:56:47 -0400

    novi zadachi lists

---
+++ Python Fundamentals – June 2018/02. Lists and Dictionaries/01. DICTs/08. Filter Base.py
@@ -0,0 +1,57 @@
+raw_data = input()
+
+employee = {}
+
+
+def is_int(n):
+    try:
+        float_n = float(n)
+        int_n = int(float_n)
+    except ValueError:
+        return False
+    else:
+        return float_n == int_n
+
+
+def is_float(n):
+    try:
+        float_n = float(n)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
+while raw_data != 'filter base':
+
+    name, user_info = raw_data.split(' -> ')
+
+    if name not in employee:
+        employee[name] = {}
+
+    if is_int(user_info):
+        employee[name]['Age'] = int(user_info)
+
+    elif is_float(user_info):
+        employee[name]['Salary'] = float(user_info)
+
+    else:
+        employee[name]['Position'] = user_info
+
+    raw_data = input()
+
+filter_by = input()
+
+# for key in employee:
+#     print(f"{key} -> {employee[key]}")
+
+if filter_by == 'Position':
+    for name in employee:
+        print(f'Name: {name}')
+        print("{}: {}".format(filter_by, employee[name][filter_by]))
+        print("=" * 20)
+else:
+    for name in sorted(employee, reverse=True):
+        print(f'Name: {name}')
+        print(f'{filter_by}: {employee[name][filter_by]}')
+        print("=" * 20)
