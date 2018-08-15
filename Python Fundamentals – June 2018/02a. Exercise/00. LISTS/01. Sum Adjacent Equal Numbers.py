46aa62e6d9c952e66d9c785f8a024a35d0c8f4ca
vlado <vgrozev@gmail.com>
2018-08-15 00:01:02 -0400

    novi zadachi dicts

---
+++ Python Fundamentals – June 2018/02a. Exercise/00. LISTS/01. Sum Adjacent Equal Numbers.py
@@ -0,0 +1,20 @@
+data = list(map(float, input().split()))
+
+count = len(data)
+i = 0
+while i < (len(data) - 1):
+    if data[i] == data[i + 1] and i >= 0:
+        data[i] += data[i + 1]
+        data.remove(data[i + 1])
+        i -= 1
+    else:
+        i += 1
+
+# print(*data)
+# for item in data:
+#     print("{0:g}".format(item), end=' ')
+
+# print(" ".join('%g' % item for item in data))
+
+print(" ".join(["{:g}".format(item) for item in data]))
+
