0283a4023347b85425fcec16f8b76225c891a387
vlado <vgrozev@gmail.com>
2018-10-04 23:39:14 -0400

    novi zadachi dicts2

--- Python Fundamentals – June 2018/02a. Exercise/01. DICTs/01. Key-Key Value-Value.py
+++ Python Fundamentals – June 2018/02a. Exercise/01. DICTs/01. Key-Key Value-Value.py
@@ -1,14 +1,9 @@
-import pprint
-
-input_key = input()
-input_value = input()
+input_key = input().strip()
+input_value = input().strip()
 
 num_pairs = int(input())
 
-final_value_list = []
 final_dict = {}
-
-pp = pprint.PrettyPrinter(indent=4)
 
 for i in range(0, num_pairs):
 
@@ -16,9 +11,22 @@ for i in range(0, num_pairs):
     current_key = pair[0]
     current_value = pair[1].split(';')
 
-    for item in current_value:
-        if input_value in item:
-            print(item)
+    # if the input_key is at least a substring of the currently processed key,
+    # loop trough the current_value list.
+    # if the input_value is contained in any of the items of the current_value list,
+    # add those items to a list which is a value for the current key.
+    # Add the current_key and this list of values as a key-value pair to the final_dict
+    if input_key in current_key:
 
+        if current_key not in final_dict:
+            final_dict[current_key] = []
 
-pp.pprint(final_dict)
+        for item in current_value:
+            if input_value in item:
+                item.strip()
+                final_dict[current_key].append(item)
+
+for key in final_dict:
+    print(f'{key}:')
+    for value in final_dict[key]:
+        print(f'-{value}')
